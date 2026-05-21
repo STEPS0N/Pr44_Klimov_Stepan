8a5f576a359dde75ca76c1762a1c6b5659feff4d
STEPS0N <stepan.klimoff@mail.ru>
2026-05-21 10:03:41 +0500

    Реализация окна пользователей. Климов

--- Users_Klimov/View/MainRoles.xaml.cs
+++ Users_Klimov/View/MainRoles.xaml.cs
@@ -20,9 +20,10 @@ namespace Users_Klimov.View
     /// </summary>
     public partial class MainRoles : Page
     {
-        public MainRoles()
+        public MainRoles(object Context)
         {
             InitializeComponent();
+            DataContext = Context;
         }
     }
 }
