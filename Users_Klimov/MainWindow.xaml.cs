052276a0524e7a95ee21c02b82221d650911866d
STEPS0N <stepan.klimoff@mail.ru>
2026-05-21 09:50:16 +0500

    Добавьте файлы проекта.

---
+++ Users_Klimov/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Users_Klimov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow init;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToRoles(object sender, MouseButtonEventArgs e)
+        {
+            frame.Navigate(new View.MainRoles());
+        }
+
+        private void ToUsers(object sender, MouseButtonEventArgs e)
+        {
+            frame.Navigate(new View.MainUsers());
+        }
+    }
+}
